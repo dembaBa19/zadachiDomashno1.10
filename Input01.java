45be99e582f5095dae46fcad407605269add3135
dembaBa19 <71441966+dembaBa19@users.noreply.github.com>
2020-10-05 13:02:59 +0300

    Add files via upload

---
+++ Input01.java
@@ -0,0 +1,23 @@
+package paki4;
+
+import javax.swing.JOptionPane;
+
+public class Input01 {
+    public static void main(String[] args) {
+        //Create a JOptionPane.
+        //Store the input as a String and print it.
+        
+        String input=JOptionPane.showInputDialog("What's your name?");
+        System.out.println(input);
+        //Parse the input as an int.
+        //Print its value +1
+        
+//        int number = Integer.parseInt(input);
+//        number++;
+//        System.out.println(number);
+        //Try creating a dialog, parsing it, and initializing an int in a single line.
+        //You should have only one semicolon (;) in this line.
+        System.out.println(Integer.parseInt(JOptionPane.showInputDialog("Enter a number"))+1);
+        
+    }
+}
